c7fd635eca7afb32b5f3e1c3f6291a6098ad0b9a
Andrey Georgitsa <andrey_georgitsa@epam.com>
2019-05-13 17:06:14 +0300

    Instance Function variable for CompletableFuture creation

--- src/test/java/Task_2_CF_Tests.java
+++ src/test/java/Task_2_CF_Tests.java
@@ -7,30 +7,28 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Task_2_CF_Tests {
 
+    private Function<String, CompletableFuture<String>> createDelayedCF = msg -> CompletableFuture.supplyAsync(() -> {
+        try {
+            TimeUnit.SECONDS.sleep((long) (Math.random() * 5));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return String.format("Hi from %s!", msg);
+    });
+
     @Test
-    public void test_anyOf(){
+    public void test_anyOf() {
         List<String> messages = Arrays.asList("CF1", "CF2", "CF3");
         List<String> result = new ArrayList<>();
 
-        List<CompletableFuture<String>> futures = messages.stream()
-                .map(msg -> CompletableFuture.supplyAsync(() -> {
-                    try {
-                        TimeUnit.SECONDS.sleep((long)(Math.random()*5));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    return String.format("Hi from %s!", msg);
-                }))
-                .collect(Collectors.toList());
-
-        CompletableFuture<Object> completableFuture = CompletableFuture.anyOf(futures.toArray(new CompletableFuture[futures.size()]))
-                .whenComplete((res, th) -> {
-                     result.add(res.toString());
-        });
+        CompletableFuture<Object> completableFuture = CompletableFuture.anyOf(messages.stream()
+                .map(createDelayedCF).toArray(CompletableFuture[]::new))
+                .whenComplete((v, th) -> result.add(v.toString()));
 
         completableFuture.join();
         Assert.assertEquals(1, result.size());
@@ -43,14 +41,14 @@ public class Task_2_CF_Tests {
         List<String> messages = Arrays.asList("CF1", "CF2", "CF3");
         List<String> result = new ArrayList<>();
         List<CompletableFuture<String>> futures = messages.stream()
-                .map(msg -> CompletableFuture.supplyAsync(() -> String.format("Hi from %s!", msg)))
+                .map(createDelayedCF)
                 .collect(Collectors.toList());
 
         CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
                 .whenComplete((v, th) -> {
                     futures.forEach(cf -> {
                         try {
-                           result.add(cf.get());
+                            result.add(cf.get());
                         } catch (InterruptedException | ExecutionException e) {
                             e.printStackTrace();
                         }
