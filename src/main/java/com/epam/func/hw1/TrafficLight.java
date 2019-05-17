4895e099903ae0d305df8ef0fe1e52e2b2915d4b
Azamat Khamzin <Azamat_Khamzin@epam.com>
2019-05-17 15:29:22 +0300

    refactor

--- src/main/java/com/epam/func/hw1/TrafficLight.java
+++ src/main/java/com/epam/func/hw1/TrafficLight.java
@@ -2,38 +2,32 @@ package com.epam.func.hw1;
 
 
 public class TrafficLight {
+
   private Color currentColor = Color.RED;
 
   public void start() {
     while (true) {
       System.out.println("Current color is: " + currentColor);
-      Thread counterThread = new Thread(counter(currentColor));
-      counterThread.start();
-      try {
-        counterThread.join();
-      } catch (InterruptedException e) {
-        throw new RuntimeException("Something went wrong!");
-      }
+      counter(currentColor);
       currentColor = getNextColor(currentColor);
     }
   }
 
   private Color getNextColor(Color currentColor) {
-    return currentColor == Color.RED ? Color.YELLOW : currentColor == Color.YELLOW ? Color.GREEN : Color.RED;
+    return currentColor == Color.RED ? Color.YELLOW
+        : currentColor == Color.YELLOW ? Color.GREEN : Color.RED;
   }
 
-  private Runnable counter(Color color) {
-    return () -> {
-      int waitingTime = color.getTimeInMilliseconds();
-      while (waitingTime != 0) {
-        waitingTime-=1000;
-        System.out.println(waitingTime/1000 + " seconds remaining.");
-        try {
-          Thread.sleep(1000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+  private void counter(Color color) {
+    int waitingTime = color.getTimeInMilliseconds();
+    while (waitingTime != 0) {
+      waitingTime -= 1000;
+      System.out.println(waitingTime / 1000 + " seconds remaining.");
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
-    };
+    }
   }
 }
