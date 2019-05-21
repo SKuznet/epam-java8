c998fe62a479dbc09290095ee3e787af47c50fb5
artsiom <artsiom_panko@epam.com>
2019-05-21 13:49:52 +0300

    Add some code

---
+++ src/main/java/com/epam/func/hm2/Main.java
@@ -0,0 +1,105 @@
+package com.epam.func.hm2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        anyOfFutureMethod();
+        allOfMethod();
+        whenCompleteMethod();
+    }
+
+    private static void anyOfFutureMethod() throws Exception {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 1";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 2";
+        });
+
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 3";
+        });
+
+        CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(future1, future2, future3);
+
+        System.out.println("Any of firts: " + anyOfFuture.get());
+    }
+
+    private static void allOfMethod() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 1";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 2";
+        });
+
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 3";
+        });
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(future1, future2, future3);
+        System.out.println(allFutures.get());
+    }
+
+    private static void whenCompleteMethod() {
+
+        StringBuilder result = new StringBuilder();
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Future 1";
+        });
+
+        CompletableFuture<String> whenCompleteFutures = future1;
+
+        whenCompleteFutures.whenComplete((resp, ex) -> {
+            if (ex != null) {
+                result.append("done");
+            }
+            System.out.println(result);
+        });
+
+    }
+}
